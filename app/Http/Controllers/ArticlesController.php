d9ef5768d21a073de7399187405bfa18f1830ff7
張 建文 <chang180@gmail.com>
2020-10-10 11:51:07 +0800

    update to day 25

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,29 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Article;
 
 class ArticlesController extends Controller
 {
-    public function index(){
-        return view('articles.index');
+public function __construct(){
+    $this->middleware('auth')->except('index');
+}
+
+    public function index()
+    {
+        // $articles = Article::orderBy('id','desc')->get();
+        $articles = Article::paginate(3);
+        return view('articles.index',['articles'=>$articles]);
     }
 
-    public function create(){
+    public function create()
+    {
         return view('articles.create');
     }
 
-    public function store(Request $request){
-$content=$request->validate([
-    'title'=>'required',
-    'content'=>'required|min:10'
-]);
+    public function store(Request $request)
+    {
 
-auth()->user()->articles()->create($content);
-return redirect()->route('root')->with('notice','文章新增成功！');
+        $content = $request->validate([
+            'title' => 'required',
+            'content' => 'required|min:10'
+        ]);
 
-
+        auth()->user()->articles()->create($content);
+        return redirect()->route('root')->with('notice', '文章新增成功！');
     }
 
-    
+    public function edit($id){
+        $article = auth()->user()->articles->find($id);
+        // $article = Article::find($id);
+        return view('article.edit',['article'=>$article]);
+    }
+    public function update(Request $request, $id){
+        $article = auth()->user()->articles->find($id);
+
+        $content = $request->validate([
+            'title' => 'required',
+            'content' => 'required|min:10'
+        ]);
+
+        $article->update($content);
+        return redirect()->route('root')->with('notice','文章更新成功');
+
+    }
 }
--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -2,5 +2,18 @@
 
 @section('main')
     <h1 class="font-thin text-4xl">文章列表</h1>
-<a href="{{ route('articles.create') }}">新增文章</a>
+    <a href="{{ route('articles.create') }}">新增文章</a>
+
+    @foreach ($articles as $article)
+        <div class="border-t border-gray-300 my-1 p-2">
+            <h2 class="font-bold text-lg">{{ $article->title }}</h2>
+            <p>
+                {{ $article->created_at }} 由 {{ $article->user->name }} 分享
+            </p>
+            {{-- <a href=" {{ route('articles.edit',['article'=>$article->id])}} ">編輯</a> --}}
+            <a href=" {{ route('articles.edit',$article)}} ">編輯</a>
+        </div>
+    @endforeach
+
+    {{ $articles->links() }}
 @endsection
